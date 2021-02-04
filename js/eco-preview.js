b9b0ad9b56b568c16da69fad99b9a892a732a71f
Stefan Van Damme <stefan.vd01@gmail.com>
2021-02-04 19:59:57 +0100

    Fixed eco preview

--- js/eco-preview.js
+++ js/eco-preview.js
@@ -32,9 +32,10 @@ function isKeyInObject(obj, key){
 	return res;
 }
 
-function getDomain(url){
 // http://data.iana.org/TLD/tlds-alpha-by-domain.txt
-	var TLDs = ["ac", "ad", "ae", "aero", "af", "ag", "ai", "al", "am", "an", "ao", "app", "aq", "ar", "arpa", "as", "asia", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "biz", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cat", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "com", "coop", "cr", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "edu", "ee", "eg", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gov", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "info", "int", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jobs", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mil", "mk", "ml", "mm", "mn", "mo", "mobi", "mp", "mq", "mr", "ms", "mt", "mu", "museum", "mv", "mw", "mx", "my", "mz", "na", "name", "nc", "ne", "net", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "org", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "pro", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "su", "sv", "sy", "sz", "tc", "td", "tel", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr", "travel", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "video", "vn", "vu", "wf", "ws", "xn--0zwm56d", "xn--11b5bs3a9aj6g", "xn--3e0b707e", "xn--45brj9c", "xn--80akhbyknj4f", "xn--90a3ac", "xn--9t4b11yi5a", "xn--clchc0ea0b2g2a9gcd", "xn--deba0ad", "xn--fiqs8s", "xn--fiqz9s", "xn--fpcrj9c3d", "xn--fzc2c9e2c", "xn--g6w251d", "xn--gecrj9c", "xn--h2brj9c", "xn--hgbk6aj7f53bba", "xn--hlcj6aya9esc7a", "xn--j6w193g", "xn--jxalpdlp", "xn--kgbechtv", "xn--kprw13d", "xn--kpry57d", "xn--lgbbat1ad8j", "xn--mgbaam7a8h", "xn--mgbayh7gpa", "xn--mgbbh1a71e", "xn--mgbc0a9azcg", "xn--mgberp4a5d4ar", "xn--o3cw4h", "xn--ogbpf8fl", "xn--p1ai", "xn--pgbs0dh", "xn--s9brj9c", "xn--wgbh1c", "xn--wgbl6a", "xn--xkc2al3hye2a", "xn--xkc2dl3a5ee0h", "xn--yfro4i67o", "xn--ygbi2ammx", "xn--zckzah", "xxx", "ye", "yt", "za", "zm", "zw"].join();
+var TLDs = ["ac", "ad", "ae", "aero", "af", "ag", "ai", "al", "am", "an", "ao", "app", "aq", "ar", "arpa", "as", "asia", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "biz", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cat", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "com", "coop", "cr", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "edu", "ee", "eg", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gov", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "info", "int", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jobs", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mil", "mk", "ml", "mm", "mn", "mo", "mobi", "mp", "mq", "mr", "ms", "mt", "mu", "museum", "mv", "mw", "mx", "my", "mz", "na", "name", "nc", "ne", "net", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "org", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "pro", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "su", "sv", "sy", "sz", "tc", "td", "tel", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr", "travel", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "video", "vn", "vu", "wf", "ws", "xn--0zwm56d", "xn--11b5bs3a9aj6g", "xn--3e0b707e", "xn--45brj9c", "xn--80akhbyknj4f", "xn--90a3ac", "xn--9t4b11yi5a", "xn--clchc0ea0b2g2a9gcd", "xn--deba0ad", "xn--fiqs8s", "xn--fiqz9s", "xn--fpcrj9c3d", "xn--fzc2c9e2c", "xn--g6w251d", "xn--gecrj9c", "xn--h2brj9c", "xn--hgbk6aj7f53bba", "xn--hlcj6aya9esc7a", "xn--j6w193g", "xn--jxalpdlp", "xn--kgbechtv", "xn--kprw13d", "xn--kpry57d", "xn--lgbbat1ad8j", "xn--mgbaam7a8h", "xn--mgbayh7gpa", "xn--mgbbh1a71e", "xn--mgbc0a9azcg", "xn--mgberp4a5d4ar", "xn--o3cw4h", "xn--ogbpf8fl", "xn--p1ai", "xn--pgbs0dh", "xn--s9brj9c", "xn--wgbh1c", "xn--wgbl6a", "xn--xkc2al3hye2a", "xn--xkc2dl3a5ee0h", "xn--yfro4i67o", "xn--ygbi2ammx", "xn--zckzah", "xxx", "ye", "yt", "za", "zm", "zw"].join();
+
+function getDomain(url){
 	url = url.replace(/.*?:\/\//g, "");
 	url = url.replace(/www./g, "");
 	var parts = url.split("/");
@@ -43,19 +44,15 @@ function getDomain(url){
 	if(parts[0] === "www" && parts[1] !== "com"){
 		parts.shift();
 	}
-	var ln = parts.length
-		, i = ln
-		, minLength = parts[parts.length - 1].length
-		, part;
+	var ln = parts.length, i = ln, minLength = parts[parts.length - 1].length, part;
 
 	// iterate backwards
-	var cond = part = parts[--i];
-	while(cond){
+	while((part = parts[--i])){
 		// stop when we find a non-TLD part
 		if(i === 0 // 'asia.com' (last remaining must be the SLD)
-                || i < ln - 2 // TLDs only span 2 levels
-                || part.length < minLength // 'www.cn.com' (valid TLD as second-level domain)
-                || TLDs.indexOf(part) < 0 // officialy not a TLD
+				|| i < ln - 2 // TLDs only span 2 levels
+				|| part.length < minLength // 'www.cn.com' (valid TLD as second-level domain)
+				|| TLDs.indexOf(part) < 0 // officialy not a TLD
 		){
 			var actual_domain = part;
 			break;
@@ -74,7 +71,6 @@ function getDomain(url){
 		actual_domain = actual_domain + tid;
 	else
 		actual_domain = actual_domain + ".com";
-
 
 	return actual_domain;
 }
@@ -113,7 +109,7 @@ if(window.location.href != totloptionspage){
 }
 
 function domcontentloaded(){
-// reset
+	// reset
 	document.getElementById("btnresetanalytics").addEventListener("click", function(){
 		chrome.storage.sync.set({"analytics":null, "siteengagement":null});
 		location.reload();
@@ -230,7 +226,7 @@ function domcontentloaded(){
 			}else{ last30days = analytics; }
 			last30days.map(function(a){ return a.name; });
 			last30days.map(function(a){ return a.details.active; });
-			last30days.map(function(a){
+			time30days = last30days.map(function(a){
 				return a.details.time;
 			});
 
@@ -311,11 +307,11 @@ function domcontentloaded(){
 			for(i in last30daysb){
 				if(last30daysb[i]){
 					var key;
-					var oma = last30daysb[i];
-					for(key in oma){
+					var omaa = last30daysb[i];
+					for(key in omaa){
 						if(Object.prototype.hasOwnProperty.call(omaa, key)){
 							if(key != "name"){ // not the date
-								var value = parseFloat(Math.round(oma[key] / 60 * 100) / 100).toFixed(2);
+								var value = parseFloat(Math.round(omaa[key] / 60 * 100) / 100).toFixed(2);
 								var app = isKeyInObject(newtablesite, key);
 								if(app == true){
 									var currentnumber = parseInt(newtablesite[key]);
@@ -348,12 +344,12 @@ function domcontentloaded(){
 			var v;
 			for(v in last30daysdomainonly){
 				if(last30daysdomainonly[v]){
-					var omaa = last30daysdomainonly[v];
-					for(key in omaa){
+					var oma = last30daysdomainonly[v];
+					for(key in oma){
 						var abc = getDomain(key);
-						if(Object.prototype.hasOwnProperty.call(omaa, key)){
+						if(Object.prototype.hasOwnProperty.call(oma, key)){
 							if(key != "name"){ // not the date
-								var valuee = parseFloat(Math.round(omaa[key] / 60 * 100) / 100).toFixed(2);
+								var valuee = parseFloat(Math.round(oma[key] / 60 * 100) / 100).toFixed(2);
 								var appe = isKeyInObject(newtablesitedomain, abc);
 								if(appe == true){
 									var currentnumbera = parseInt(newtablesitedomain[abc]);
@@ -361,7 +357,7 @@ function domcontentloaded(){
 									var resta = currentnumbera;
 									newtablesitedomain[abc] = resta;
 								}else{
-									newtablesitedomain[abc] = value;
+									newtablesitedomain[abc] = valuee;
 								}
 							}
 						}
@@ -387,8 +383,8 @@ function domcontentloaded(){
 		// CSP
 		Chart.platform.disableCSSInjection = true;
 		// --- Begin chart1
-		var ctx = document.getElementById("myChart").getContext("2d");
-		new Chart(ctx, {
+		var ctx1 = document.getElementById("myChart").getContext("2d");
+		new Chart(ctx1, {
 			type: "line",
 			data: {
 				datasets: [{
@@ -491,7 +487,7 @@ function domcontentloaded(){
 	function createtables(){
 		// --- Begin chart2
 		var testlove = false;
-		var ctx = document.getElementById("myCharttwo").getContext("2d");
+		var ctx2 = document.getElementById("myCharttwo").getContext("2d");
 		var sitenamedomain = [];
 		var sitevaluedomain = [];
 		var key;
@@ -534,7 +530,7 @@ function domcontentloaded(){
 			legend: {display:false, position:"top"}
 		};
 
-		new Chart(ctx, {
+		new Chart(ctx2, {
 			type: "doughnut",
 			data: data,
 			options: options
@@ -563,7 +559,7 @@ function domcontentloaded(){
 			row.appendChild(cellb);
 			table.appendChild(row);
 		}
-		// --- End table1
+	// --- End table1
 	}
 	function showcard(){
 		// --- Begin Card
