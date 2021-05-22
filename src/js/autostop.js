0659668de0032c56251e9fd9d1b20151bf7d1614
Stefan Van Damme <stefan.vd01@gmail.com>
2021-05-22 12:27:49 +0200

    Update autostop.js

--- src/js/autostop.js
+++ src/js/autostop.js
@@ -151,9 +151,7 @@ chrome.storage.sync.get(["autostop", "autostoponly", "autostopDomains", "autosto
 			firstautostopremove[0].parentNode.removeChild(firstautostopremove[0]);
 		}
 
-		var visualvideos = document.getElementsByTagName("video");
-		var selectedvideo = null;
-		var i, l = visualvideos.length;
+		var visualvideos = document.getElementsByTagName("video"), selectedvideo = null i, l = visualvideos.length;
 		for(i = 0; i < l; i++){
 			selectedvideo = visualvideos[i];
 			selectedvideo.setAttribute("data-videonum", i);
