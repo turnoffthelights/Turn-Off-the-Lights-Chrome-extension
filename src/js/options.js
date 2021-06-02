0b08689b80a7002b4577abdeb2c30f7290e13ed8
Stefan Van Damme <stefan.vd01@gmail.com>
2021-06-02 20:18:09 +0200

    Update options.js

--- src/js/options.js
+++ src/js/options.js
@@ -464,27 +464,20 @@ function read_options(){
 		}
 	}
 
-	var settingscolorarray = ["lightcolor", "ambilightcolorhex", "ambilight1colorhex", "ambilight2colorhex", "ambilight3colorhex", "ambilight4colorhex", "colora", "colorb", "nightmodebck", "nightmodetxt", "nightmodehyperlink", "videovolumecolor", "videotoolcolor", "titleinvertcolor", "nightmodebutton"];
-	function setcoloroptions(a){
-		for(var iset = 0; iset < settingscolorarray.length; iset++){
-			if(a[settingscolorarray[iset]]){ $(settingscolorarray[iset]).value = a[settingscolorarray[iset]]; }
+	var settingsinputrarray = ["lightcolor", "ambilightcolorhex", "ambilight1colorhex", "ambilight2colorhex", "ambilight3colorhex", "ambilight4colorhex", "colora", "colorb", "nightmodebck", "nightmodetxt", "nightmodehyperlink", "videovolumecolor", "videotoolcolor", "titleinvertcolor", "nightmodebutton", "lightimage", "enterpassword", "spotlightradius", "linearsq", "intervallina", "intervallinb", "reflectionamount", "videovolumesteps", "nmbegintime", "nmendtime", "ecosavertime", "begintime", "endtime", "nightmodeswitchhidetime", "playrateamount", "drawatmosfps", "hovervideoamount", "visopacity", "autoplaydelaytime", "maxquality", "interval", "ambilightrangeblurradius", "ambilightrangespreadradius"];
+	function setinputoptions(a){
+		for(var iset = 0; iset < settingsinputrarray.length; iset++){
+			if(a[settingsinputrarray[iset]]){ $(settingsinputrarray[iset]).value = a[settingsinputrarray[iset]]; }
 		}
 	}
 
-	var settingsvaluearray = ["lightimage", "enterpassword", "spotlightradius", "linearsq", "intervallina", "intervallinb", "reflectionamount", "videovolumesteps", "nmbegintime", "nmendtime", "ecosavertime", "begintime", "endtime", "nightmodeswitchhidetime", "playrateamount", "drawatmosfps", "hovervideoamount", "visopacity", "autoplaydelaytime", "maxquality", "interval", "ambilightrangeblurradius", "ambilightrangespreadradius"];
-	function setvalueoptions(a){
-		for(var iset = 0; iset < settingsvaluearray.length; iset++){
-			if(a[settingsvaluearray[iset]]){ $(settingsvaluearray[iset]).value = a[settingsvaluearray[iset]]; }
-		}
-	}
 
-	var a = settingscheckboxarray, b = settingscolorarray, c = settingsvaluearray;
-	var allsettings = a.concat(b, c);
+	var a = settingscheckboxarray, b = settingsinputrarray;
+	var allsettings = a.concat(b);
 	//---
 	chrome.storage.sync.get(["firstDate", "interval", "lightcolor", "lightimage", "lightimagea", "lightimagen", "autoplay", "playlist", "flash", "head", "fadein", "fadeout", "infobar", "sharebutton", "likebutton", "readera", "readern", "shortcutlight", "eyea", "eyen", "suggestions", "videoheadline", "eastereggs", "contextmenus", "viewcount", "eyealist", "mousespotlighto", "mousespotlightc", "mousespotlighta", "nighttime", "begintime", "endtime", "addvideobutton", "likebar", "ambilight", "ambilightrangeblurradius", "ambilightrangespreadradius", "mousespotlightt", "ambilightfixcolor", "ambilightvarcolor", "ambilightcolorhex", "ambilight4color", "ambilight1colorhex", "ambilight2colorhex", "ambilight3colorhex", "ambilight4colorhex", "password", "enterpassword", "noflash", "hardflash", "ecosaver", "ecosavertime", "dynamic", "dynamic1", "dynamic2", "dynamic3", "dynamic4", "dynamic5", "dynamic6", "dynamic7", "dynamic8", "dynamic9", "dynamic10", "hoveroptiondyn5", "autoplayonly", "blur", "maxquality", "autowidthyoutube", "customqualityyoutube", "cinemaontop", "alllightsoff", "spotlightradius", "atmosphereonly", "optionskipremember", "nighttheme", "nightonly", "nightenabletheme", "autoplaydelay", "autoplaydelaytime", "motion", "lightimagelin", "linearsq", "colora", "intervallina", "colorb", "intervallinb", "speech", "speechlang", "speechcountry", "atmosvivid", "countremember", "excludedDomains", "autoplayDomains", "atmosphereDomains", "nightDomains", "cammotiononly", "speechonly", "cammotionDomains", "speechDomains", "autoplaychecklistwhite", "autoplaychecklistblack", "reviewedlastonversion", "applastonversion", "autostop", "autostoponly", "autostopchecklistwhite", "autostopchecklistblack", "nightmodechecklistwhite", "nightmodechecklistblack", "autostopDomains", "nighthover", "nmtopleft", "nmtopright", "nmbottomright", "nmbottomleft", "nmcustom", "nightactivetime", "nmbegintime", "nmendtime", "lampandnightmode", "eyechecklistblack", "eyechecklistwhite", "nightmodebck", "nightmodetxt", "mobilelastonversion", "no360youtube", "videotool", "reflection", "reflectionamount", "videotoolonly", "videotoolchecklistwhite", "videotoolchecklistblack", "videotoolDomains", "nightmodehyperlink", "block60fps", "videovolume", "videovolumecolor", "videovolumesteps", "videovolumelabel", "icon", "visopacity", "videotoolcolor", "hovervideo", "hovervideoamount", "mousespotlights", "drawatmosfps", "aplay", "apause", "astop", "videozoom", "playrate", "playrateamount", "speedtoolbar", "atmosontotlmode", "titleinvertcolor", "vpause", "darkbrowsertheme", "autostopred", "autostoptrans", "videovolumeposa", "videovolumeposb", "videovolumeposc", "videovolumehold", "multiopacityDomains", "multiopacall", "multiopacsel", "firstsawrate", "videovolumealt", "nightmodebydomain", "nightmodebypage", "introduce", "seeanalytics", "nightmodegesture", "nightmodeswitchhide", "nightmodeswitchhidetime", "atmosfpsauto", "atmosfpsmanual", "videovolumeonly", "videovolumechecklistwhite", "videovolumechecklistblack", "videovolumeDomains", "videovolumescrolla", "videovolumescrollb", "videovolumescrollc", "videovolumeposd", "videovolumepose", "nightmodebutton", "firstsawyoutube", "gamepad"], function(items){
 		setcheckboxoptions(items);
-		setcoloroptions(items);
-		setvalueoptions(items);
+		setinputoptions(items);
 
 		if(items["speechlang"]){ $("select_language").selectedIndex = items["speechlang"]; updateCountry(); }
 		if(items["speechcountry"]){ $("select_dialect").value = items["speechcountry"]; }
