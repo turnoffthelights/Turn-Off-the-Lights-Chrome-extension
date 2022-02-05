88a38f24a95229f89adaa5dcb237de995deb1977
Stefan Van Damme <20127018+stefanvd@users.noreply.github.com>
2022-02-05 13:55:26 +0100

    Update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -796,7 +796,7 @@ https://bugs.chromium.org/p/chromium/issues/detail?id=1217895
 OK Done IMPROVEMENT atmos vivid, the glow fade in and fade out
 OK Done IMPROVEMENT double click lamp button action
 OK Done IMPROVEMENT panel information panel design
-OK Done REMOVEd Speech and Camera -> not possile anymore because of service worker background page
+OK Done REMOVED Speech and Camera -> not possile anymore because of service worker background page
 + IMPROVE rate box to 5 stars
 + issue: no YouTube video detection right to left layout issue ARAB
 + issue PIP-mode
